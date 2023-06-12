68eeb89b0876f97f9fdf2dbfdc4ae5083c57bdc2
JP-Cardoso <jhonpoal@hotmail.com>
2023-06-12 10:53:10 -0300

    feat: chats

--- src/app/components/dashboard/chart-in-line/chart-in-line.component.ts
+++ src/app/components/dashboard/chart-in-line/chart-in-line.component.ts
@@ -12,7 +12,7 @@ export class ChartInLineComponent {
 	  animationEnabled: true,
 	  theme: "light2",
 	  title:{
-		text: "Actual vs Projected Sales"
+		text: "Temperatura ao Dia"
 	  },
 	  axisX:{
 		valueFormatString: "D MMM"
@@ -57,26 +57,26 @@ export class ChartInLineComponent {
 			{ x: new Date(2021, 8, 15), y: 79 }
 		]
 	  }, {
-		type: "line",
-		showInLegend: true,
-		name: "Actual Sales",
-		dataPoints: [
-			{ x: new Date(2021, 8, 1), y: 60 },
-			{ x: new Date(2021, 8, 2), y: 57 },
-			{ x: new Date(2021, 8, 3), y: 51 },
-			{ x: new Date(2021, 8, 4), y: 56 },
-			{ x: new Date(2021, 8, 5), y: 54 },
-			{ x: new Date(2021, 8, 6), y: 55 },
-			{ x: new Date(2021, 8, 7), y: 54 },
-			{ x: new Date(2021, 8, 8), y: 69 },
-			{ x: new Date(2021, 8, 9), y: 65 },
-			{ x: new Date(2021, 8, 10), y: 66 },
-			{ x: new Date(2021, 8, 11), y: 63 },
-			{ x: new Date(2021, 8, 12), y: 67 },
-			{ x: new Date(2021, 8, 13), y: 66 },
-			{ x: new Date(2021, 8, 14), y: 56 },
-			{ x: new Date(2021, 8, 15), y: 64 }
-		]
+		// type: "line",
+		// showInLegend: true,
+		// name: "Actual Sales",
+		// dataPoints: [
+		// 	{ x: new Date(2021, 8, 1), y: 60 },
+		// 	{ x: new Date(2021, 8, 2), y: 57 },
+		// 	{ x: new Date(2021, 8, 3), y: 51 },
+		// 	{ x: new Date(2021, 8, 4), y: 56 },
+		// 	{ x: new Date(2021, 8, 5), y: 54 },
+		// 	{ x: new Date(2021, 8, 6), y: 55 },
+		// 	{ x: new Date(2021, 8, 7), y: 54 },
+		// 	{ x: new Date(2021, 8, 8), y: 69 },
+		// 	{ x: new Date(2021, 8, 9), y: 65 },
+		// 	{ x: new Date(2021, 8, 10), y: 66 },
+		// 	{ x: new Date(2021, 8, 11), y: 63 },
+		// 	{ x: new Date(2021, 8, 12), y: 67 },
+		// 	{ x: new Date(2021, 8, 13), y: 66 },
+		// 	{ x: new Date(2021, 8, 14), y: 56 },
+		// 	{ x: new Date(2021, 8, 15), y: 64 }
+		// ]
 	  }]
 	}	
 }
